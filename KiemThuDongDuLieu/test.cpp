b2bda4dc0f5a32d0846f212df84706dc7769824b
dinhngocduc1311 <22028166@vnu.edu.vn>
2025-03-24 18:22:04 +0700

    Kiem thu dong du lieu

---
+++ KiemThuDongDuLieu/test.cpp
@@ -0,0 +1,29 @@
+#include <gtest/gtest.h>
+#include "tinhTien.h"
+
+TEST(TinhTienTest, InvalidInputs) {
+    EXPECT_EQ(tinhTien(-1, 5000, 30, 1), -1);
+    EXPECT_EQ(tinhTien(5000, 10001, 30, 1), -1);
+    EXPECT_EQ(tinhTien(5000, 10001, 30, 1), -1);
+    EXPECT_EQ(tinhTien(5000, 5000, 151, 0), -1);
+    EXPECT_EQ(tinhTien(5000, 5000, 151, 0), -1);
+}
+
+TEST(TinhTienTest, ValidInputs) {
+    EXPECT_FLOAT_EQ(tinhTien(0, 5000, 30, 0), 5000 * 20);
+    EXPECT_FLOAT_EQ(tinhTien(0, 5000, 30, 0), 5000 * 20);
+    EXPECT_FLOAT_EQ(tinhTien(0, 5000, 30, 0), 5000 * 20);
+    EXPECT_FLOAT_EQ(tinhTien(5000, 5000, 1, 1), (5000 * 10 + 5000 * 20) * 0.8);
+    EXPECT_FLOAT_EQ(tinhTien(0, 5000, 30, 0), 5000 * 20);
+    EXPECT_FLOAT_EQ(tinhTien(5000, 5000, 149, 0), (5000 * 10 + 5000 * 20) * 0.9);
+    EXPECT_FLOAT_EQ(tinhTien(0, 5000, 30, 0), 5000 * 20);
+    EXPECT_FLOAT_EQ(tinhTien(5000, 5000, 1, 1), (5000 * 10 + 5000 * 20) * 0.8);
+    EXPECT_FLOAT_EQ(tinhTien(0, 5000, 30, 0), 5000 * 20);
+    EXPECT_FLOAT_EQ(tinhTien(5000, 5000, 149, 0), (5000 * 10 + 5000 * 20) * 0.9);
+    EXPECT_FLOAT_EQ(tinhTien(0, 5000, 30, 0), 5000 * 20);
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
