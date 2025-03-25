5020f5fd04d1ddb869c3d8e07bcb7880b5bffa06
dinhngocduc1311 <22028166@vnu.edu.vn>
2025-03-25 22:04:35 +0700

    update test.cpp

--- KiemThuDongDuLieu/test.cpp
+++ KiemThuDongDuLieu/test.cpp
@@ -4,23 +4,13 @@
 TEST(TinhTienTest, InvalidInputs) {
     EXPECT_EQ(tinhTien(-1, 5000, 30, 1), -1);
     EXPECT_EQ(tinhTien(5000, 10001, 30, 1), -1);
-    EXPECT_EQ(tinhTien(5000, 10001, 30, 1), -1);
-    EXPECT_EQ(tinhTien(5000, 5000, 151, 0), -1);
     EXPECT_EQ(tinhTien(5000, 5000, 151, 0), -1);
 }
 
 TEST(TinhTienTest, ValidInputs) {
     EXPECT_FLOAT_EQ(tinhTien(0, 5000, 30, 0), 5000 * 20);
-    EXPECT_FLOAT_EQ(tinhTien(0, 5000, 30, 0), 5000 * 20);
-    EXPECT_FLOAT_EQ(tinhTien(0, 5000, 30, 0), 5000 * 20);
     EXPECT_FLOAT_EQ(tinhTien(5000, 5000, 1, 1), (5000 * 10 + 5000 * 20) * 0.8);
-    EXPECT_FLOAT_EQ(tinhTien(0, 5000, 30, 0), 5000 * 20);
     EXPECT_FLOAT_EQ(tinhTien(5000, 5000, 149, 0), (5000 * 10 + 5000 * 20) * 0.9);
-    EXPECT_FLOAT_EQ(tinhTien(0, 5000, 30, 0), 5000 * 20);
-    EXPECT_FLOAT_EQ(tinhTien(5000, 5000, 1, 1), (5000 * 10 + 5000 * 20) * 0.8);
-    EXPECT_FLOAT_EQ(tinhTien(0, 5000, 30, 0), 5000 * 20);
-    EXPECT_FLOAT_EQ(tinhTien(5000, 5000, 149, 0), (5000 * 10 + 5000 * 20) * 0.9);
-    EXPECT_FLOAT_EQ(tinhTien(0, 5000, 30, 0), 5000 * 20);
 }
 
 int main(int argc, char **argv) {
